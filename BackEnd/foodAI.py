94d998156f1730d24ed5bf00d63f1fa00b0679c4
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2020-01-18 18:45:49 -0500

    import artificialintelligence as ai

--- BackEnd/foodAI.py
+++ BackEnd/foodAI.py
@@ -1,68 +1,34 @@
 """uses word2vec to give a list of similar foods."""
 
-import gensim
-import tempfile
-from gensim import models
-import gensim.downloader as api
-
 import spacy
 
 
-
 class FoodAi:
+    """
+import artificialintelligence as ai
+ai.dotheworkforme()
+    """
+
     def __init__(self):
         """Load spacy model from dataset"""
         self.model = spacy.load("en_core_web_md")
 
+    def get_similar_foods(self, word: str, topn: int):
+        """
+        returns a list (no duplicates) of similar foods
+        """
 
-        loaded = api.load('word2vec-google-news-300')
-        for i, word in enumerate(loaded.vocab):
-            if i == 10:
-                break
-            print(word)
-
-        with tempfile.NamedTemporaryFile(prefix='gensim-model-',
-                                         delete=False) as tmp:
-            temporary_filepath = tmp.name
-            loaded.save(temporary_filepath)
-            #
-            # The model is now safely stored in the filepath.
-            # You can copy it to other machines, share it with others, etc.
-            #
-            # To load a saved model:
-            print(temporary_filepath)
-        print("Generated model!")
-        return loaded
-
-
-def load_model():
-    """
-    loads model from file.
-    """
-    print("Loading model from file...")
-
-    w = models.Word2Vec.load_word2vec_format(
-        'GoogleNews-vectors-negative300.bin', binary=True)
-    w = models.KeyedVectors.load_word2vec_format('model', binary=True)
-    return w # gensim.models.Word2Vec.load("model")
-
-
-# def get_similar_foods(word: str, topn: int):
-#     """
-#     returns a list (no duplicates) of similar foods
-#     """
-#
-#     word = nlp.vocab[str(word)]
-#     queries = [w for w in word.vocab if
-#                w.is_lower == word.is_lower and w.prob >= -15]
-#     by_similarity = sorted(queries, key=lambda w: word.similarity(w),
-#                            reverse=True)
-#     return [(w.lower_, w.similarity(word)) for w in by_similarity[:topn + 1]
-#             if w.lower_ != word.lower_]
+        word = self.model.vocab[str(word)]
+        queries = [w for w in word.vocab if
+                   w.is_lower == word.is_lower and w.prob >= -15]
+        by_similarity = sorted(queries, key=lambda w: word.similarity(w),
+                               reverse=True)
+        return [(w.lower_, w.similarity(word)) for w in by_similarity[:topn + 1]
+                if w.lower_ != word.lower_]
 
 
 if __name__ == '__main__':
-    model = load_model()
+    thing = FoodAi()
+    thing.get_similar_foods('hot dog', 15)
     #
     # get_similar_foods(model, 'pasta', 10)
-
