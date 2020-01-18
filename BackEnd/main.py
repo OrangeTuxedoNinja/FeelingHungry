05b1b4bcd4840f809c7c6a5b1a82e1541068a5e5
Quajak <jasper.gerigk@gmx.net>
2020-01-17 23:34:11 -0500

    Created initial classses for foods

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
+from BackEnd.food import Food
+from BackEnd.foodProducer import FoodProducer
+
 app = Flask(__name__, template_folder="../FrontEnd")
+foodset = FoodProducer()
 
 path = "http://35.203.43.136:5000/"
 
@@ -14,6 +18,10 @@ def render_static(page_name):
 @app.route("/")
 def main_page():
     return redirect("http://127.0.0.1:5000/index")
+
+@app.route("/api/search/<string:food>")
+def search_food(food):
+    return foodset.get_food(food).toJson()
 
 
 if __name__ == '__main__':
