f7e2ee7e1152b23e194c0864df3a9ede681aea37
Quajak <jasper.gerigk@gmx.net>
2020-01-17 23:12:17 -0500

    Fix file structure Add basic server

---
+++ BackEnd/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__, template_folder="../FrontEnd")
+
+path = "http://127.0.0.1:5000/"
+
+@app.route('/main')
+def hello_world():
+    return 'test!'
+
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+
+@app.route("/")
+def main_page():
+    return redirect("http://127.0.0.1:5000/index")
+
+
+if __name__ == '__main__':
+    app.run()
